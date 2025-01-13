d52feffd75e7cbde3c790926ca91d5a2e741901f
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-01-13 13:45:20 +0900

    Update.

--- Date.rb
+++ Date.rb
@@ -1,5 +1,25 @@
 require 'date'
 
-puts Time.now
+class WatchDate
+  attr_reader :app
+  
+  def initialize
+    @app = puts Time.now
+  end
 
-# _posts date timestamp.
+  def remove
+    remove_instance_variable(:@app)
+  end
+end
+
+# About Exception, begin ~ rescue ~ ensure.
+begin
+  WatchDate.new.remove
+rescue StandardError => e
+  puts e.backtrace
+ensure
+  GC.compact
+end
+
+# _posts date timestamp
+# 2025-01-13 13:39:34 +0900
